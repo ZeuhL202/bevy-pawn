3ec939d2bb058d899d4d48d97494a676a0b41d61
ZeuhL202 <olmandkent@gmail.com>
2025-03-02 20:35:33 +0900

    mouse fix wip

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,13 @@
 use std::f64::consts::PI;
 use rand::Rng;
 use bevy::{
-    input::mouse::MouseWheel,
-    prelude::*,
-    sprite::Wireframe2dPlugin
+    input::mouse::{self, MouseWheel}, prelude::*, sprite::Wireframe2dPlugin
 };
 
 const WINDOW_WIDTH: f32 = 800.0;
 const WINDOW_HEIGHT: f32 = 600.0;
 const TILE_SIZE: f32 = 50.0;
+const TILE_RANGE: i32 = 10;
 
 #[derive(PartialEq)]
 enum PawnState {
@@ -55,6 +54,13 @@ impl Default for Settings {
     }
 }
 
+#[derive(Resource)]
+struct MousePosition {
+    window: Option<Vec2>,
+    field: Option<Vec2>,
+    tile: Option<Vec2>,
+}
+
 #[derive(Component)]
 struct DebugLog(String);
 
@@ -64,48 +70,43 @@ impl DebugLog {
     }
 }
 
-#[derive(Event)]
-struct Click();
-
-#[derive(Event)]
-struct RightClick(Vec2);
-
-fn get_tile_pos(pos: Vec2) -> Vec2 {
-    let x = (pos.x / TILE_SIZE).round() * TILE_SIZE;
-    let y = (pos.y / TILE_SIZE).round() * TILE_SIZE;
+fn tile_rounded_field_position(position: Vec2) -> Vec2 {
+    let x = (position.x / TILE_SIZE).round() * TILE_SIZE;
+    let y = (position.y / TILE_SIZE).round() * TILE_SIZE;
     Vec2::new(x, y)
 }
 
-fn current_mouse_pos(pos: Vec2, transform: Vec3, scale: f32) -> Vec2 {
-    Vec2 {
-        x: (pos.x + transform.x / scale - WINDOW_WIDTH / 2.0) * scale,
-        y: (pos.y - transform.y / scale - WINDOW_HEIGHT / 2.0) * -scale,
-    }
-}
-
 fn setup(
+    mut commands: Commands,
     mut windows: Query<&mut Window>,
-    mut cmds: Commands,
-    asset_server: Res<AssetServer>,
 ) {
     // ウィンドウ
     let mut window = windows.single_mut();
     window.resolution.set(WINDOW_WIDTH, WINDOW_HEIGHT);
+    window.resizable = false;
     window.title = "Bevy".to_string();
 
-    cmds.spawn((
-        Camera2d,
-        Transform::from_xyz(250.0, 250.0, 0.0),
-    ));
+    let camera_position = 25.0 * TILE_RANGE as f32;
 
+    commands.spawn((
+        Camera2d,
+        Transform::from_xyz(camera_position, camera_position, 0.0),
+    ));
+}
+
+fn spawn_pawn(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+) {
     let mut rng = rand::rng();
     for _ in 0..5 {
-        let rng_x = rng.random_range(0..10) as f32 * TILE_SIZE;
-        let rng_y = rng.random_range(0..10) as f32 * TILE_SIZE;
-        cmds.spawn((
+        let rng_x = rng.random_range(0..TILE_RANGE) as f32 * TILE_SIZE;
+        let rng_y = rng.random_range(0..TILE_RANGE) as f32 * TILE_SIZE;
+
+        commands.spawn((
             Sprite::from_image(asset_server.load("pawn.png")),
             Transform::from_xyz(rng_x, rng_y, 1.0)
-                .with_scale(Vec3::splat(5.0 / TILE_SIZE)),
+                .with_scale(Vec3::splat(TILE_SIZE / 512.0)),
             Thing {
                 hovered: false,
                 _selected: false,
@@ -117,55 +118,61 @@ fn setup(
     }
 }
 
-fn click_handle(
-    mouse_button_input: Res<ButtonInput<MouseButton>>,
-    camera: Single<(&mut OrthographicProjection, &Transform), With<Camera2d>>,
+fn send_resouce_mouse_position(
     window: Query<&Window>,
-    mut events: EventWriter<Click>,
-    mut debug_log: Query<&mut DebugLog>,
+    camera: Query<(&Transform, &OrthographicProjection), With<Camera2d>>,
+    mut mouse_position: ResMut<MousePosition>,
 ) {
-    if let Some(window) = window.get_single().ok() {
-        if let Some(mut pos) = window.cursor_position() {
-            pos = current_mouse_pos(pos, camera.1.translation, camera.0.scale);
-            if mouse_button_input.just_pressed(MouseButton::Left) {
-                events.send(Click());
-            }
-            if let Ok(mut debug_log) = debug_log.get_single_mut() {
-                debug_log.add(format!("MousePosition x:{:.2}, y:{:.2}", pos.x, pos.y));
-            }
-        } else {
-            if let Ok(mut debug_log) = debug_log.get_single_mut() {
-                debug_log.add("MousePosition NoCursor".to_string());
-            }
-        }
+    let Some(window) = window.get_single().ok() else { return };
+    let Some(camera) = camera.get_single().ok() else { return };
+
+    let transform = camera.0.translation;
+    let scale = camera.1.scale;
+
+    if let Some(position) = window.cursor_position() {
+        let field_position = Vec2 {
+            x: (position.x + transform.x / scale - WINDOW_WIDTH / 2.0) * scale,
+            y: (position.y - transform.y / scale - WINDOW_HEIGHT / 2.0) * -scale,
+        };
+
+        mouse_position.window = Some(Vec2::new(position.x, position.y));
+        mouse_position.field = Some(field_position);
+        mouse_position.tile = Some(field_position / TILE_SIZE);
+    } else {
+        mouse_position.window = None;
+        mouse_position.field = None;
+        mouse_position.tile = None;
     }
 }
 
-fn right_click_handle(
-    mouse_button_input: Res<ButtonInput<MouseButton>>,
-    camera: Single<(&mut OrthographicProjection, &Transform), With<Camera2d>>,
-    window: Query<&Window>,
-    mut events: EventWriter<RightClick>,
+fn log_mouse_position(
+    mouse_position: Res<MousePosition>,
+    mut debug_log: Query<&mut DebugLog>,
 ) {
-    if let Some(window) = window.get_single().ok() {
-        if let Some(mut pos) = window.cursor_position() {
-            pos = current_mouse_pos(pos, camera.1.translation, camera.0.scale);
-            if mouse_button_input.just_pressed(MouseButton::Right) {
-                events.send(RightClick(pos));
-            }
-        }
-    }
+    let Some(mut debug_log) = debug_log.get_single_mut().ok() else { return };
+
+    let string =
+        if let Some(position) = mouse_position.window {
+            format!("x:{:.2}, y:{:.2}", position.x, position.y)
+        } else {
+            "NoCursor".to_string()
+        };
+
+    debug_log.add(format!("MousePosition {}", string));
 }
 
 fn select_pawn(
-    mut event: EventReader<Click>,
+    mouse_button_input: Res<ButtonInput<MouseButton>>,
+    asset_server: Res<AssetServer>,
     selecters: Query<Entity, With<Selecter>>,
     things: Query<(&Thing, Entity), (With<Thing>, Without<Selecter>)>,
-    mut cmds: Commands,
-    asset_server: Res<AssetServer>,
+    mut commands: Commands,
 ) {
-    if let None = event.read().next() { return }; // if no click event then return
-    selecters.iter().for_each(|e| cmds.entity(e).despawn()); // Remove all selecters
+    // if no click then return
+    if !mouse_button_input.just_pressed(MouseButton::Left) { return };
+
+    // Remove all selecters
+    selecters.iter().for_each(|e| commands.entity(e).despawn());
 
     // get the hovered thing
     let Some(hovered) = things.iter().filter(|(c, _)| c.hovered).next() else { return };
@@ -173,56 +180,52 @@ fn select_pawn(
 
     // create a selecter as a child of the hovered things
     let image_hundle = asset_server.load("frame.png");
-    let child = cmds.spawn((
+    let child = commands.spawn((
         Sprite::from_image(image_hundle),
         Transform::from_xyz(0.0, 0.0, 1.5),
         Selecter,
     )).id();
 
-    cmds.entity(hovered).add_child(child);
+    commands.entity(hovered).add_child(child);
 }
 
 fn let_move_pawn(
-    mut event: EventReader<RightClick>,
+    mouse_button_input: Res<ButtonInput<MouseButton>>,
     selecter: Query<Entity, With<Selecter>>,
     mut pawns: Query<(&Children, &mut Pawn), With<Pawn>>,
 ) {
-    let Some(pos) = event.read().next() else { return };
-    let tile_pos = get_tile_pos(pos.0);
+    if !mouse_button_input.just_pressed(MouseButton::Right) { return };
+    let field_position = field_position(position.0);
 
-    pawns.iter_mut().filter(|(c, _)| c.iter().any(|&e| selecter.get(e).is_ok())).for_each(|(_, mut pawn)| {
-        pawn.state = PawnState::Move(tile_pos);
-    });
+    for (_, mut pawn) in pawns.iter_mut().filter(|(c, _)| c.iter().any(|&e| selecter.get(e).is_ok())) {
+        pawn.state = PawnState::Move(field_position);
+    };
 }
 
 fn move_pawn(
     mut pawns: Query<(&mut Pawn, &mut Transform)>,
     time: Res<Time>,
 ) {
-    for (mut pawn, mut pawn_pos) in pawns.iter_mut() {
-        if let PawnState::Move(dest_pos) = pawn.state {
-            let pawn_y = pawn_pos.translation.y;
-            let dest_y = dest_pos.y;
-            let pawn_x = pawn_pos.translation.x;
-            let dest_x = dest_pos.x;
+    for (mut component, mut transform) in pawns.iter_mut() {
+        // if pawn's state is not Move then continue
+        let PawnState::Move(destination) = component.state else { continue };
 
-            let theta = (pawn_y - dest_y).atan2(pawn_x - dest_x) + PI as f32;
-            let dist = ((pawn_y - dest_y).powi(2) + (pawn_x - dest_x).powi(2)).sqrt();
-            let power =
-                if dist < 1.0 {
-                    pawn.state = PawnState::Idle;
-                    pawn_pos.translation.x += (dest_x - pawn_x) * time.delta_secs();
-                    pawn_pos.translation.y += (dest_y - pawn_y) * time.delta_secs();
-                    return
-                } else if dist < 10.0 {
-                    dist / 10.0 * 100.0
-                } else {
-                    100.0
-                };
+        // straight angle to the destination
+        let theta = (transform.translation.y - destination.y).atan2(transform.translation.x - destination.x) + PI as f32;
+        let distance = transform.translation.distance(Vec3::new(destination.x, destination.y, 1.0));
 
-            pawn_pos.translation.x += theta.cos() * power * time.delta_secs();
-            pawn_pos.translation.y += theta.sin() * power * time.delta_secs();
+        if distance < 0.1 {
+            component.state = PawnState::Idle;
+            transform.translation = Vec3::new(destination.x, destination.y, 1.0);
+            return;
         }
+
+        // The speed increase is capped at 10.0
+        let speed = distance.min(10.0) * 10.0;
+
+        // The intersection of the straight line to the destination and the unit circle with itself as the origin is the coordinate to move forward.
+        transform.translation.x += theta.cos() * speed * time.delta_secs();
+        transform.translation.y += theta.sin() * speed * time.delta_secs();
     };
 }
 
@@ -232,75 +235,49 @@ fn is_thing_hovered(
     window: Query<&Window>,
 ) {
     let Some(window) = window.get_single().ok() else { return };
-    let Some(cursor_pos) = window.cursor_position() else { return };
+    let Some(cursor_position) = window.cursor_position() else { return };
 
     for (transform, mut component) in things.iter_mut() {
-        let cursor_pos = current_mouse_pos(cursor_pos, camera.single().1.translation, camera.single().0.scale);
+        let cursor_position = field_mouse_position(cursor_position, camera.single().1.translation, camera.single().0.scale);
         let thing_x = transform.translation.x;
         let thing_y = transform.translation.y;
         let half_size = TILE_SIZE / 2.0;
 
-        if ((thing_x - half_size)..(thing_x + half_size)).contains(&cursor_pos.x)
-        && ((thing_y - half_size)..(thing_y + half_size)).contains(&cursor_pos.y) {
-            component.hovered = true;
-        } else {
-            component.hovered = false;
-        }
+        // The area is a square centered on the current coordinate system.
+        component.hovered = ((thing_x - half_size)..(thing_x + half_size)).contains(&cursor_position.x)
+                         && ((thing_y - half_size)..(thing_y + half_size)).contains(&cursor_position.y);
     }
 }
 
 fn spawn_tile(
-    mut cmds: Commands,
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
 ) {
-    let c: fn(i32) -> f32 = |i| i as f32 / 10.0;
     let t: fn(i32) -> f32 = |i| i as f32 * TILE_SIZE;
 
-    for i in 0..10 {
-        for j in 0..10 {
-            cmds.spawn((
-                Sprite::from_color(
-                    Color::srgb(c(i), c(j), 0.5),
-                    Vec2 { x: TILE_SIZE, y: TILE_SIZE }
-                ),
-                Transform::from_xyz(t(i), t(j), 0.0),
-            ));
-            cmds.spawn((
-                Text2d::new(format!("({},{})", i, j),),
-                TextFont {
-                    font_size: 10.0,
-                    ..default()
-                },
-                Transform::from_xyz(t(i), t(j), 0.5)
+    let image_hundle = asset_server.load("debug_tile.png");
+
+    for i in 0..TILE_RANGE {
+        for j in 0..TILE_RANGE {
+            commands.spawn((
+                Sprite::from_image(image_hundle.clone()),
+                Transform::from_xyz(t(i), t(j), 0.0)
+                    .with_scale(Vec3::splat(TILE_SIZE / 512.0)),
+            ))
+            .with_child((
+                Text2d::new(format!("({},{})", i, j)),
+                Transform::from_xyz(0.0, 0.0, 0.5)
+                    .with_scale(Vec3::splat(2.5)),
             ));
         }
     }
 }
 
-fn move_camera(
-    mut camera: Query<&mut Transform, With<Camera2d>>,
+fn log_camera_position(
+    camera: Query<&Transform, With<Camera2d>>,
     mut debug_log: Query<&mut DebugLog>,
-    keys: Res<ButtonInput<KeyCode>>,
-    settings: Res<Settings>,
-    time: Res<Time>,
 ) {
-    let mut camera = camera.single_mut();
-    let mut dx: f32 = 0.0;
-    let mut dy: f32 = 0.0;
-    let camera_speed = settings.camera_move_speed;
-
-    if keys.pressed(KeyCode::KeyW) { dy += 1.0 * camera_speed }
-    if keys.pressed(KeyCode::KeyA) { dx -= 1.0 * camera_speed }
-    if keys.pressed(KeyCode::KeyS) { dy -= 1.0 * camera_speed }
-    if keys.pressed(KeyCode::KeyD) { dx += 1.0 * camera_speed }
-    if keys.pressed(KeyCode::ShiftLeft) {
-        dx *= 3.0;
-        dy *= 3.0;
-    }
-
-    camera.translation.x += dx * time.delta_secs();
-    camera.translation.y += dy * time.delta_secs();
-
-    // Add a camera position to the log
+    let Some(camera) = camera.get_single().ok() else { return };
     let Some(mut debug_log) = debug_log.get_single_mut().ok() else { return };
     debug_log.add(format!(
         "CameraPosition x:{:.2}, y:{:.2}",
@@ -309,87 +286,100 @@ fn move_camera(
     ));
 }
 
-fn zoom_camera(
-    mut camera: Query<&mut OrthographicProjection, With<Camera2d>>,
-    mut evr_scroll: EventReader<MouseWheel>,
+fn move_camera(
+    mut camera: Query<&mut Transform, With<Camera2d>>,
     keys: Res<ButtonInput<KeyCode>>,
-    mut debug_log: Query<&mut DebugLog>,
     settings: Res<Settings>,
     time: Res<Time>,
 ) {
-    use bevy::input::mouse::MouseScrollUnit;
+    let Some(mut camera) = camera.get_single_mut().ok() else { return };
+    let mut distance = Vec2::ZERO;
 
-    let mut camera = camera.single_mut();
-    let evr_scroll_first = evr_scroll.read().next();
+    if keys.pressed(KeyCode::KeyW) { distance.y += 1.0 }
+    if keys.pressed(KeyCode::KeyA) { distance.x -= 1.0 }
+    if keys.pressed(KeyCode::KeyS) { distance.y -= 1.0 }
+    if keys.pressed(KeyCode::KeyD) { distance.x += 1.0 }
 
-    let Some(ev) = evr_scroll_first else { return };
-    match ev.unit {
-        MouseScrollUnit::Line => {
-            let shift_speed = if keys.pressed(KeyCode::ShiftLeft) { 10.0 } else { 1.0 };
-            let ds = ev.y * settings.camera_zoom_speed * shift_speed * 20.0 * time.delta_secs();
-            let post_scale = camera.scale + ds;
+    let shift_multiplier = if keys.pressed(KeyCode::ShiftLeft) { 10.0 } else { 1.0 };
 
-            if 0.1 < post_scale && post_scale < 10.0 {
-                camera.scale += ds;
-            }
-        },
-        MouseScrollUnit::Pixel => {
-            let shift_speed = if keys.pressed(KeyCode::ShiftLeft) { 10.0 } else { 1.0 };
-            let ds = ev.y * settings.camera_zoom_speed * shift_speed * time.delta_secs();
-            let post_scale = camera.scale + ds;
-
-            if 0.1 < post_scale && post_scale < 10.0 {
-                camera.scale += ds;
-            }
-        },
+    if distance.length_squared() > 0.0 {
+        distance = distance.normalize();
     }
 
-    // Add a camera scale to the log
+    camera.translation.x += distance.x * settings.camera_move_speed * shift_multiplier * time.delta_secs();
+    camera.translation.y += distance.y * settings.camera_move_speed * shift_multiplier * time.delta_secs();
+}
+
+fn log_camera_scale(
+    camera: Query<&OrthographicProjection, With<Camera2d>>,
+    mut debug_log: Query<&mut DebugLog>,
+) {
+    let Some(camera) = camera.get_single().ok() else { return };
     let Some(mut debug_log) = debug_log.get_single_mut().ok() else { return };
+
     debug_log.add(format!("CameraScale: {:.2}", camera.scale));
+}
+
+fn zoom_camera(
+    mut camera: Query<&mut OrthographicProjection, With<Camera2d>>,
+    mut event_read_scroll: EventReader<MouseWheel>,
+    keys: Res<ButtonInput<KeyCode>>,
+    settings: Res<Settings>,
+    time: Res<Time>,
+) {
+    let Some(mut camera) = camera.get_single_mut().ok() else { return };
+    let Some(ev) = event_read_scroll.read().next() else { return };
+
+    let shift_speed = if keys.pressed(KeyCode::ShiftLeft) { 10.0 } else { 1.0 };
+    let ds = ev.y * settings.camera_zoom_speed * shift_speed * 20.0 * time.delta_secs();
+    let post_scale = camera.scale + ds;
+
+    if (0.1..10.0).contains(&post_scale) {
+        camera.scale += ds;
+    }
 }
 
 fn output_log(
     mut text: Query<&mut Text, With<DebugLog>>,
     mut buf: Query<&mut DebugLog>,
 ) {
-    let (Some(mut text), Some(mut buf)) = (text.get_single_mut().ok(), buf.get_single_mut().ok()) else { return };
+    let Some(mut text) = text.get_single_mut().ok() else { return };
+    let Some(mut buf) = buf.get_single_mut().ok() else { return };
     text.0 = buf.0.clone();
     buf.0 = "--- LOG ---".to_string();
 }
 
 fn toggle_log(
     entity: Query<Entity, With<DebugLog>>,
-    mut cmds: Commands,
+    mut commands: Commands,
     asset_server: Res<AssetServer>,
     keys: Res<ButtonInput<KeyCode>>,
 ) {
     if !keys.just_pressed(KeyCode::KeyH) { return }
 
-    let Some(e) = entity.get_single().ok() else {
-        let font_handle = asset_server.load("fonts/Menlo-Regular.ttf");
-        cmds.spawn((
+    if let Some(e) = entity.get_single().ok() {
+        commands.entity(e).despawn();
+    } else {
+        commands.spawn((
             Text::new(""),
             TextFont {
-                font: font_handle,
+                font: asset_server.load("fonts/Menlo-Regular.ttf"),
                 ..default()
             },
             BackgroundColor(Color::srgba(0.0, 0.0, 0.0, 0.5)),
             DebugLog("--- LOG ---".to_string())
         ));
-        return
-    };
-    cmds.entity(e).despawn();
+    }
 }
 
 fn close_on_q(
-    mut cmds: Commands,
+    mut commands: Commands,
     window: Query<(Entity, &Window)>,
     input: Res<ButtonInput<KeyCode>>,
 ) {
-    let Ok((window, _focus)) = window.get_single() else { return };
+    let Some((window, _focus)) = window.get_single().ok() else { return };
     if input.just_pressed(KeyCode::KeyQ) {
-        cmds.entity(window).despawn();
+        commands.entity(window).despawn();
     }
 }
 
@@ -400,24 +390,26 @@ fn main() {
             Wireframe2dPlugin,
         ))
         .insert_resource(Settings::default())
-        .add_event::<Click>()
-        .add_event::<RightClick>()
         .add_systems(Startup, (
             setup,
             spawn_tile,
+            spawn_pawn,
         ))
         .add_systems(Update, (
-            click_handle,
-            right_click_handle,
             select_pawn,
             let_move_pawn,
             move_pawn,
             is_thing_hovered,
             move_camera,
             zoom_camera,
+            close_on_q,
+        ))
+        .add_systems(Update, (
+            log_mouse_position,
+            log_camera_position,
+            log_camera_scale,
             output_log,
             toggle_log,
-            close_on_q,
         ).chain())
         .run();
 }
